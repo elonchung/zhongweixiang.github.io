9c16f9efc8f2cae381fdf31d98e4f2d60f289687
zhongweixiang <331845158@qq.com>
2020-05-07 15:05:22 +0800

    Site updated: 2020-05-07 15:05:19

--- js/application.js
+++ js/application.js
@@ -1,3 +1,3 @@
-// build time:Mon May 21 2018 13:14:04 GMT+0800 (中国标准时间)
+// build time:Thu May 07 2020 15:05:17 GMT+0800 (GMT+08:00)
 $(function(){$('[data-toggle="tooltip"]').tooltip();if(typeof $.fn.slimScroll!="undefined"){$(".sidebar .slimContent").slimScroll({height:$(window).height(),color:"rgba(0,0,0,0.15)",size:"5px",position:"right"})}$("#collapseToc").on("shown.bs.collapse",function(){if(typeof $.fn.slimScroll!="undefined"){$(".sidebar .slimContent").slimScroll().on("slimscroll")}});$(".geopattern").each(function(){$(this).geopattern($(this).data("pattern-id"))});var t=$("#nav-main").okayNav({swipe_enabled:false});$("[data-stick-bottom]").keepInView({fixed:false,parentClass:"has-sticky",customClass:"sticky",trigger:"bottom",zindex:42,edgeOffset:0});$("[data-stick-top]").keepInView({fixed:true,parentClass:"has-sticky",customClass:"sticky",trigger:"top",zindex:42,edgeOffset:0})});
 //rebuild by neat 
